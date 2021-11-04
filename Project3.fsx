af4e583086a4438e0a63c5d6bdad0f432d632489
Michael Perez <40220887+michaelperez023@users.noreply.github.com>
2021-11-03 23:12:18 -0400

    Update Project3.fsx

--- Project3.fsx
+++ Project3.fsx
@@ -12,6 +12,7 @@ let system = ActorSystem.Create("System")
 let r = Random()
 let nodeDict = new Dictionary<bigint, IActorRef>()
 
+// Switch up the order of inputs
 type Message =
     | BossStart
     | CheckPredecessor
@@ -32,7 +33,6 @@ let ranStr n =
     let sz = Array.length chars in String(Array.init n (fun _ -> chars.[r.Next sz]))
 
 let NodeActor (mailbox:Actor<_>) =
-    // Declare variables we will be using
     let mutable numRequests = 0
     let mutable messagesReceived = 0
     let mutable id = bigint(-1)
@@ -41,13 +41,11 @@ let NodeActor (mailbox:Actor<_>) =
     let mutable successor = bigint(-1)
     let mutable successors = new List<bigint>()
     let mutable m = -1
-    let mutable predecessorCheck:bool = true
     let mutable numHops = 0
+    let mutable predecessorCheck:bool = true
     let mutable boss = mailbox.Self
 
-    // Routing mechanism to pick the slot a new node will fit under in our chord network
     let routeNode(hash':bigint, id':bigint, predecessor':bigint, successor':bigint, fingerTable':List<bigint>, successors':List<bigint>) = 
-        // Copy all the fingers and successors from the finger table and sort them to decide new slot
         let mutable fingers = List<bigint>()
         for i in fingerTable' do
             fingers.Add(i) |> ignore
@@ -55,27 +53,18 @@ let NodeActor (mailbox:Actor<_>) =
             fingers.Add(i) |> ignore
         fingers.Sort()
         
-        let mutable slot = id'
-        
-        // In the first if statement we want to see a few things
-        // Is the hash less than or equal to the ID
-        // Is the hash greater than the ID and predecessor while the predecessor is greater than the ID
-        // Is the hash greater than the predecessor while the ID is greater than the predecessor and the hash and the predecesor is not uninitialized
+        let mutable destination = id'
         if ((((hash' <= id') || (hash' > id' && (hash' > predecessor'))) && (predecessor' > id') || (((hash' > predecessor') && (hash' <= id')))) && (predecessor' < id')) && (predecessor' <> bigint(-1)) then
-            slot <- id'
-        // If the initial conditions are not met, we want to see if the hash is less than the first item or if it is greater than the last item
+            destination <- id'
         else if hash' < fingers.Item(0) || hash' > fingers.Item(fingers.Count - 1) then
-            slot <- fingers.Item(fingers.Count - 1)
-        // We then want to see if the successor is greater than the ID and the hash, all the while the hash is greater than the ID
-        // Or if the ID is greater than the successor and the hash while the hass is less than the successor
+            destination <- fingers.Item(fingers.Count - 1)
         else if (id' < successor' && (hash' > id' && hash' <= successor')) || (id' > successor' && ((hash' < id' && hash' <= successor') || hash' > id')) then
-            slot <- successor'
-        // If none of the conditions are met we will traverse the finger table and compare the current value with the hash and allocate it on to the new slot
+            destination <- successor'
         else
             for i in 0..fingers.Count - 1 do
                 if hash' > fingers.Item(i) && hash' <= fingers.Item(i+1) then
-                    slot <- fingers.Item(i)
-        slot
+                    destination <- fingers.Item(i)
+        destination
 
     let rec loop () = actor {
         let! message = mailbox.Receive()
@@ -83,10 +72,6 @@ let NodeActor (mailbox:Actor<_>) =
             boss <- mailbox.Sender()
 
         match message with
-        // We first want to see if the node can join the network
-        | Join(node) -> 
-            nodeDict.Item(node) <! GetSuccessor(id)
-        // A node will then be initialized with all the values obtained from the boss node about the network
         | Initialize(id', predecessor', successor', m', numRequests', successors', fingerTable') -> 
             id <- id'
             predecessor <- predecessor'
@@ -95,56 +80,38 @@ let NodeActor (mailbox:Actor<_>) =
             numRequests <- numRequests'
             successors <- successors'
             fingerTable <- fingerTable'
-        // Values could be updated if there is  change in the number of requests, the byte size m, or the ID of a node
-        | Update(m', numRequests', id') -> 
-            m <- m'
-            numRequests <- numRequests'
-            id <- id'
-        // The node can then start on the network and begin communicating with other nodes and being an active part of the network
         | NodeStart -> 
             // Generate random text from some random string
             let mutable randomText = ranStr 5
             let hash = abs(bigint(System.Text.Encoding.ASCII.GetBytes(randomText) |> HashAlgorithm.Create("SHA1").ComputeHash)) % bigint(Math.Pow(2.0, float(m)))
-            let slot = routeNode(hash, id, predecessor, successor, fingerTable, successors)
-            nodeDict.Item(slot) <! Request(randomText, 0, hash, id)
+            let destination = routeNode(hash, id, predecessor, successor, fingerTable, successors)
+            nodeDict.Item(destination) <! Request(randomText, 0, hash, id)
             system.Scheduler.ScheduleTellOnce(TimeSpan.FromSeconds(1.), mailbox.Self, NodeStart) // send request once/second
-        // we want to send a request to a node to and see if it can route a message accross the network
+        | Update(m', numRequests', id') -> 
+            m <- m'
+            numRequests <- numRequests'
+            id <- id'
         | Request(message', hops', hash', id') -> 
-            let slot = routeNode(hash', id, predecessor, successor, fingerTable, successors)
-            let hopsCounter = hops' + 1
-            if slot = id then
-                nodeDict.Item(id') <! RequestRouted(hopsCounter)
+            let destination = routeNode(hash', id, predecessor, successor, fingerTable, successors)
+            let newHops = hops' + 1
+            if destination = id then
+                nodeDict.Item(id') <! RequestRouted(newHops)
             else
-                nodeDict.Item(slot) <! Request(message', hopsCounter, hash', id')
-        // Check the amount of hops it took for a request to get routed through the network
+                nodeDict.Item(destination) <! Request(message', newHops, hash', id')
         | RequestRouted(hops') ->
             numHops <- numHops + hops'
             messagesReceived <- messagesReceived + 1
             if messagesReceived = numRequests then
                 boss <! Complete(numHops, id)
-        // Check if the predecessor exists
-        | CheckPredecessor ->
-            if predecessor <> bigint(-1) then
-                if not predecessorCheck then
-                    predecessor <- bigint(-1)
-                else
-                    nodeDict.Item(predecessor) <! SuccessorPredecessorCheck
-                    predecessorCheck <- false
-            system.Scheduler.ScheduleTellOnce(TimeSpan.FromSeconds(0.5), mailbox.Self, CheckPredecessor)
-        // Send a message from the predecessor indicating to the successor where it is located
-        | SuccessorPredecessorCheck -> 
-            nodeDict.Item(successor) <! PredecessorCheck
-        | PredecessorCheck -> 
-            predecessorCheck <- true
-        // Obtain information on where the successor of a node may be at
+        | Join(node) -> 
+            nodeDict.Item(node) <! GetSuccessor(id)
         | GetSuccessor(node) -> 
-            let slot = routeNode(node, id, predecessor, successor, fingerTable, successors)
-            if slot = id && predecessor <> bigint(-1) then
+            let destination = routeNode(node, id, predecessor, successor, fingerTable, successors)
+            if destination = id && predecessor <> bigint(-1) then
                 nodeDict.Item(node) <! SuccessorCheck(id, predecessor, successors)
                 predecessor <- node
             else
-                nodeDict.Item(slot) <! GetSuccessor(node)
-        // Check to see where the successor is based on the ID, Predecessor, and other Successors
+                nodeDict.Item(destination) <! GetSuccessor(node)
         | SuccessorCheck(successor', predecessor', successors') ->
             fingerTable.Add(successor')
             successors.Add(successor')
@@ -154,6 +121,18 @@ let NodeActor (mailbox:Actor<_>) =
                 successors.Add(i)
             successors.Sort()
             nodeDict.Item(id) <! NodeStart
+        | CheckPredecessor ->
+            if predecessor <> bigint(-1) then
+                if not predecessorCheck then
+                    predecessor <- bigint(-1)
+                else
+                    nodeDict.Item(predecessor) <! SuccessorPredecessorCheck
+                    predecessorCheck <- false
+            system.Scheduler.ScheduleTellOnce(TimeSpan.FromSeconds(0.5), mailbox.Self, CheckPredecessor)
+        | SuccessorPredecessorCheck -> 
+            nodeDict.Item(successor) <! PredecessorCheck
+        | PredecessorCheck -> 
+            predecessorCheck <- true
         | _ -> ()
         return! loop ()
     }
@@ -186,8 +165,7 @@ let BossActor numNodesInput numRequests (mailbox:Actor<_>) =
         let! message = mailbox.Receive()
         match message with
         | BossStart -> // Start boss
-            // Assign the identifier length of m, used to identify nodes
-            let m = int(ceil((Math.Log(float(numNodes), 2.0))) * 3.0)
+            let m = int(ceil((Math.Log(float(numNodes), 2.0))) * 3.0) // Assign m
             printfn "m: %i" m
 
             // Iterate through each node in the total number of nodes we have
@@ -200,62 +178,52 @@ let BossActor numNodesInput numRequests (mailbox:Actor<_>) =
                 nodeDict.Add(id, spawn system ("Node" + string(id)) NodeActor)
                 nodeIdDict.Add(i, id)
 
-            // Now create a list of all the nodeIDs that were generated and then sort it
-            let mutable nodeIdList = new List<bigint>()
+            let mutable nodeIdList = new List<bigint>() // create list of nodeIDs and sort it
             for i in nodeIdDict.Values do
                 nodeIdList.Add(i)
             nodeIdList.Sort()
 
-            // Not sure why the indexing begins at 0 now instead of 1
             // Iterate through each item again from the total number of nodes we have
-            // Gather all the information we will need to start a new node (Lists, Tables, Indexs, etc.)
+            // Gather all the information we will need to start a new node (Lists, Tables, Indices, etc.)
             for i in 0..numNodes-1 do
-                // Now we assign values that will be used for the fingerTable and the successors
-                let mutable fingerTableSet = new List<bigint>()
+                // Assign values that will be used for the fingerTable and the successors
                 let mutable fingerTable = new List<bigint>()
                 let mutable successors = new List<bigint>()
                 for j in 0..m-1 do 
                     // Get neighbor in chord
                     let neighbor = nodeIdDict.Item(i+1) + bigint(int(Math.Pow(2.0, float(j)))) % bigint(Math.Pow(2.0, float(m)))
-                    let mutable biggerFound:bool = false;
-                    
+
+                    if neighbor > nodeIdList.Item(nodeIdList.Count - 1) then // Re-arrange neighbors
+                        fingerTable.Add(nodeIdList.Item(0)) |> ignore
+
+                    let mutable found:bool = false;
                     // Add a bigger node, if found, to the chord
                     for k in 0..nodeIdList.Count-1 do
-                        if neighbor <= nodeIdList.Item(k) && not biggerFound then
-                            fingerTableSet.Add(nodeIdList.Item(k)) |> ignore
-                            biggerFound <- true
-                    
-                    // This re-arranges the neighbors
-                    if neighbor > nodeIdList.Item(nodeIdList.Count - 1) then
-                        fingerTableSet.Add(nodeIdList.Item(0)) |> ignore
-                
-                // FingerTableSet seems to be a sorted fingertable with the hash values
-                for l in fingerTableSet do
-                    fingerTable.Add(l)
+                        if not found && neighbor <= nodeIdList.Item(k) then
+                            fingerTable.Add(nodeIdList.Item(k)) |> ignore
+                            found <- true
                 fingerTable.Sort()
-
-                // Gets sorted index list of all the nodes based off their IDs
-                let nodeIndexSortedList = nodeIdList.IndexOf(nodeIdDict.Item(i+1))
-                
-                // Create a list of all the successors based off the newly sorted node index and ID list
-                for m in 1..int((ceil(Math.Log(float(numNodes), 2.0)))) do
-                    let successorIndex = (nodeIndexSortedList + m) % nodeIdList.Count
-                    successors.Add(nodeIdList.Item(successorIndex))
                 
                 // Set the predecessor based on the node ID list and the location of the items in the node
                 let mutable predecessor = bigint(-1);
+                let mutable successor = bigint(-1);
+
                 if nodeIdList.IndexOf(nodeIdDict.Item(i + 1)) > 0 then
                     predecessor <- nodeIdList.Item(nodeIdList.IndexOf(nodeIdDict.Item(i+1)) - 1)
                 else
                     predecessor <- nodeIdList.Item(nodeIdList.Count - 1)
-
                 // Set what the successor will be at the current moment for the node based on the ID list
-                // Could be combined with the logic of collecting the successor list
-                let mutable successor = bigint(-1);
                 if nodeIdList.IndexOf(nodeIdDict.Item(i + 1)) + 1 < nodeIdList.Count then
                     successor <- nodeIdList.Item(nodeIdList.IndexOf(nodeIdDict.Item(i+1)) + 1)
                 else
                     successor <- nodeIdList.Item(0)
+
+                // Gets sorted index list of all the nodes based off their IDs
+                let nodeIndicesSorted = nodeIdList.IndexOf(nodeIdDict.Item(i+1))
+                
+                // Create successors list based off the newly sorted node index and ID list
+                for m in 1..m/3 do
+                    successors.Add(nodeIdList.Item((nodeIndicesSorted + m) % nodeIdList.Count))
                 // Done setting all the values, lists, and indexes that each node will hold
                 // Initialize a node with the data that we currently have available
                 nodeDict.Item(nodeIdDict.Item(i + 1)) <! Initialize((nodeIdDict.Item(i+1)), predecessor, successor, m, requests, successors, fingerTable)
@@ -264,9 +232,8 @@ let BossActor numNodesInput numRequests (mailbox:Actor<_>) =
             for i in 1..numNodes do
                 nodeDict.Item(nodeIdDict.Item(i)) <! NodeStart
 
-            // Iterate through nodes left
-            for i in (numNodes+1)..(numNodes+numNodesLeft) do
-                // Assign their name, hash and ID similarly as we have done before
+            for i in (numNodes+1)..(numNodes+numNodesLeft) do // Iterate through nodes left
+                // Assign the name, hash and ID similarly as done before
                 let name = "peer" + (i.ToString())
                 let hash = System.Text.Encoding.ASCII.GetBytes(name) |> HashAlgorithm.Create("SHA1").ComputeHash
                 let id = abs(bigint(hash)) % bigint(Math.Pow(2.0, double(m)))
@@ -275,16 +242,14 @@ let BossActor numNodesInput numRequests (mailbox:Actor<_>) =
                 nodeDict.Add(id, spawn system ("Node" + string(id)) NodeActor)
                 nodeIdDict.Add(i, id)
 
-                // Once they are added to the Dict and IdDict we want to update that value
-                nodeDict.Item(nodeIdDict.Item(i)) <! Update((m, requests, nodeIdDict.Item(i)))
+                nodeDict.Item(nodeIdDict.Item(i)) <! Update((m, requests, nodeIdDict.Item(i))) // update nodes left
             
-            let mutable nodeIdListNew = new List<bigint>()
+            let mutable nodeIdListFinal = new List<bigint>()
             for i in nodeIdDict.Values do
-                nodeIdListNew.Add(i)
-            nodeIdListNew.Sort()
+                nodeIdListFinal.Add(i)
+            nodeIdListFinal.Sort()
             for i in (numNodes+1)..(numNodes+numNodesLeft) do
-                let mutable rNode = nodeIdListNew.Item(r.Next(nodeIdList.Count - 1))
-                nodeDict.Item(nodeIdDict.Item(i)) <! Join(rNode)
+                nodeDict.Item(nodeIdDict.Item(i)) <! Join(nodeIdListFinal.Item(r.Next(nodeIdList.Count - 1)))
         | Complete(hops, node) -> // Node completion check
             // Print node that just finished if number of completed nodes is less than total 
             printfn "Node %A completed after %d hops. %d nodes have completed." node hops completedNodes
